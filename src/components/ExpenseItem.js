5c57b9127c4a1e1192e9e07ed56347c364109643
Vivek Kumar <mevivek3620@gmail.com>
2021-06-11 09:19:17 +0530

    First Day

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){
 
     return (
         <div className = "expense-item">
-            <div className = "date">{props.date}</div>
-            <div className = "desc">
-                 <h2 className = "item">{props.title}</h2>
-                 <div className = "amount">${props.amount}</div>
+            <ExpenseDate date = {props.date}/>
+            <div className = "expense-item__description">
+                 <h2>{props.title}</h2>
+                 <div className = "expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
